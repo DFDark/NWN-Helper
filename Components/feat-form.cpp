8b33b69592547016bb0f813e89e898ec56966851
DFDark <hamidkhairy@seznam.cz>
2019-07-29 21:00:13 +0200

    smallfix

--- Components/feat-form.cpp
+++ Components/feat-form.cpp
@@ -32,30 +32,30 @@ FeatForm::FeatForm(wxWindow* parent, TwoDA::Friendly::TwoDARow* row)
     cancel_button = new wxButton(panel, wxID_CANCEL, wxString("Cancel"));
 
     Centre();
-    
+
     wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
-    
+
     wxBoxSizer* first_row = new wxBoxSizer(wxHORIZONTAL);
-    
+
     wxBoxSizer* label_sizer = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer* name_sizer = new wxBoxSizer(wxVERTICAL);
-    
+
     label_sizer->Add(label_label);
     label_sizer->Add(label, 1, wxEXPAND|wxALL);
     name_sizer->Add(name_label);
     name_sizer->Add(name, 1, wxEXPAND|wxALL);
-    
+
     first_row->Add(label_sizer);
     first_row->Add(name_sizer);
-    
-    main_sizer->SetSizer(first_row);
-    
+
+    main_sizer->Add(first_row);
+
     wxBoxSizer* control_button_sizer = new wxBoxSizer(wxHORIZONTAL);
-    
-    control_button_sizer->Add(cancel_button
+
+    control_button_sizer->Add(cancel_button);
     control_button_sizer->Add(ok_button);
-    main_sizer->SetSizer(control_button_sizer, 0, wxALIGN_RIGHT|wxRIGHT|wxBOTTOM, 2);
-    
+    main_sizer->Add(control_button_sizer, 0, wxALIGN_RIGHT|wxRIGHT|wxBOTTOM, 2);
+
     panel->SetSizer(main_sizer);
 
     InitFormValues();
